9c6c1fce5ef060c757fd375aff8f0595f1ba9223
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-09-11 08:33:32 -0400

    Lift state from LightSwitchButton component to App.js and implement dark mode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 import AngryButton from './components/AngryButton';
 import CounterButton from './components/CounterButton';
@@ -6,13 +7,16 @@ import LightSwitchButton from './components/LightSwitchButton';
 import TextRepeaterButton from './components/TextRepeaterButton';
 
 function App() {
+  const [ light, setLight ] = useState("off");
+  const dark = (light === "off") ? "dark" : "";
+
   return (
-    <div className="App">
+    <div className={`App ${dark}`}>
       <h1>Fancy Buttons!</h1>
       <section>
         <AngryButton />
         <CounterButton />
-        <LightSwitchButton />
+        <LightSwitchButton light={light} setLight={setLight}/>
         <TextRepeaterButton />
       </section>
     </div>
