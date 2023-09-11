234c2c8813b6d57509f30e06b0cebf3f0ac254a4
Rebecca Ariss <rebeccamarionruth@gmail.com>
2023-09-11 09:43:39 -0400

    Refactor AngryButton with useReducer hook

--- src/components/AngryButton.js
+++ src/components/AngryButton.js
@@ -1,19 +1,12 @@
-import { useState } from "react";
+import { useReducer } from "react";
 
 function AngryButton(props) {
-  const [ anger, setAnger ] = useState(0);
-
-  // Could refactor with ternary operator here:
-  const handleClick = (amount) => {
-    if (anger < 1) {
-      setAnger(anger + amount);
-    } else {
-      setAnger(0);
-    }
-  }
+  const [ anger, dispatch ] = useReducer((anger, action) => {
+    return anger > 1 ? 0 : anger + action;
+  }, 0);
 
   return (
-    <button style={{ backgroundColor: `rgba(255,0,0,${anger})` }} onClick={handleClick} className="AngryButton">
+    <button style={{ backgroundColor: `rgba(255,0,0,${anger})` }} onClick={() => dispatch(0.1)} className="AngryButton">
       {/* If you have NOT reached the maximum: */}
       {anger < 1 && <span>Don't click me too much! </span>}
       {/* If you HAVE reached the maximum: */}
